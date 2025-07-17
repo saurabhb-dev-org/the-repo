7c78c637347eca43f7121796ec3987f60f38cf19
saurabhb-dev <saurabh.banawar@gmail.com>
2025-07-17 12:03:39 +0530

    Update index.js
    test the hook

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, pipeline - edited maliciously !');
+    res.send('Hello, pipeline - test the hook !');
 });
 
 app.listen(PORT, () => {
